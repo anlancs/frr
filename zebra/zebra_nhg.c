3448193e03a915d41d60d14de0af5f759ce28420
anlan_cs <vic.lan@pica8.com>
2022-01-30 19:44:35 -0500

    zebra: let /32 host route with same IP cross VRF
    Contraints of nexthop is too strict for same host route. The same host route
    should be able to cross VRF.
    
    `/32 host routes with same IP` are the routes with same destination
    address and gateway address.
    
    Before this fix, `/32` host routes are allowed to have nexthop. Since different
    `/32` host routes can cross VRFs, same `/32` should also can.
    
    There is nothing special.
    Since different VRFs can have the same IP, we want to leak only one host with
    the same IP to other VRF.
    
    Different VRFs have the hosts with the same IP.
    One FRR box has two VRFs, vrf1 and vrf2.  vrf1 is with 1.1.1.0/24,
    and vrf2 is also with 1.1.1.0/24. We only want 1.1.1.1/32 of vrf2 leak to vrf1.
    Then we set ip route 1.1.1.1/32 next-vrf vrf2 1.1.1.1  in vrf vrf1 context.
    
    Signed-off-by: anlan_cs <vic.lan@pica8.com>

--- zebra/zebra_nhg.c
+++ zebra/zebra_nhg.c
@@ -1966,7 +1966,7 @@ static int resolve_backup_nexthops(const struct nexthop *nexthop,
  */
 static int nexthop_active(struct nexthop *nexthop, struct nhg_hash_entry *nhe,
 			  const struct prefix *top, int type, uint32_t flags,
-			  uint32_t *pmtu)
+			  uint32_t *pmtu, vrf_id_t vrf_id)
 {
 	struct prefix p;
 	struct route_table *table;
@@ -2066,8 +2066,8 @@ static int nexthop_active(struct nexthop *nexthop, struct nhg_hash_entry *nhe,
 		 && nexthop->gate.ipv4.s_addr == top->u.prefix4.s_addr)
 		|| (top->family == AF_INET6 && top->prefixlen == IPV6_MAX_BITLEN
 		    && memcmp(&nexthop->gate.ipv6, &top->u.prefix6,
-			      IPV6_MAX_BYTELEN)
-			       == 0))) {
+			      IPV6_MAX_BYTELEN) == 0))
+	    && nexthop->vrf_id == vrf_id) {
 		if (IS_ZEBRA_DEBUG_RIB_DETAILED)
 			zlog_debug(
 				"        :%s: Attempting to install a max prefixlength route through itself",
@@ -2361,6 +2361,7 @@ static unsigned nexthop_active_check(struct route_node *rn,
 	const struct prefix *p, *src_p;
 	struct zebra_vrf *zvrf;
 	uint32_t mtu = 0;
+	vrf_id_t vrf_id;
 
 	srcdest_rnode_prefixes(rn, &p, &src_p);
 
@@ -2389,10 +2390,12 @@ static unsigned nexthop_active_check(struct route_node *rn,
 		goto skip_check;
 	}
 
+
+	vrf_id = zvrf_id(rib_dest_vrf(rib_dest_from_rnode(rn)));
 	switch (nexthop->type) {
 	case NEXTHOP_TYPE_IFINDEX:
 		if (nexthop_active(nexthop, nhe, &rn->p, re->type,
-				   re->flags, &mtu))
+				   re->flags, &mtu, vrf_id))
 			SET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
 		else
 			UNSET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
@@ -2401,7 +2404,7 @@ static unsigned nexthop_active_check(struct route_node *rn,
 	case NEXTHOP_TYPE_IPV4_IFINDEX:
 		family = AFI_IP;
 		if (nexthop_active(nexthop, nhe, &rn->p, re->type,
-				   re->flags, &mtu))
+				   re->flags, &mtu, vrf_id))
 			SET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
 		else
 			UNSET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
@@ -2409,7 +2412,7 @@ static unsigned nexthop_active_check(struct route_node *rn,
 	case NEXTHOP_TYPE_IPV6:
 		family = AFI_IP6;
 		if (nexthop_active(nexthop, nhe, &rn->p, re->type,
-				   re->flags, &mtu))
+				   re->flags, &mtu, vrf_id))
 			SET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
 		else
 			UNSET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
@@ -2420,7 +2423,7 @@ static unsigned nexthop_active_check(struct route_node *rn,
 			family = AFI_IP6;
 
 		if (nexthop_active(nexthop, nhe, &rn->p, re->type,
-				   re->flags, &mtu))
+				   re->flags, &mtu, vrf_id))
 			SET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
 		else
 			UNSET_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE);
